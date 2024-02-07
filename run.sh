502b1c4a50c1584e80f604dd09ecd4b99237ea2e
Your Name <you@example.com>
2024-02-07 07:11:43 +0330

    fixed bugs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 killall gunicorn
-gunicorn --bind 0.0.0.0:8000 config.wsgi -c config/gunicorn.conf.py --daemon && gunicorn --bind 0.0.0.0:8001 config.wsgi_panel -c config/gunicorn.conf.py --daemon && echo "gunicorn started :)"
+venv/bin/gunicorn --bind 0.0.0.0:8000 config.wsgi -c config/gunicorn.conf.py --daemon && venv/bin/gunicorn --bind 0.0.0.0:8001 config.wsgi_panel -c config/gunicorn.conf.py --daemon && echo "gunicorn started :)"
