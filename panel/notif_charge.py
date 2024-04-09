a8e412ba724c65bc7455585af9e49ca646ccd9bb
Your Name <you@example.com>
2024-04-09 13:58:10 +0330

    add message to end charge

--- panel/notif_charge.py
+++ panel/notif_charge.py
@@ -20,16 +20,17 @@ from time import sleep
 while True:
     for user in User.objects.all():
         amount = Wallet.objects.get(user=user).amount
-        servers: ServerRent = []
+        servers = []
         for server in ServerRent.objects.filter(is_active=True):
             time_expire = datetime.datetime.now(timezone('Asia/Tehran')) + datetime.timedelta(days=1)
             if server.expire.timestamp() < time_expire.timestamp():
                 servers.append(server)
         
-        price = 0
-        for server in servers:
-            price += server.server.price_daily if server.payment_duration == 'daily' else server.server.price_monthly
-        
-        if price > amount:
-            print(user.phone, amount, price)
+        if servers.__len__() > 0:
+            price = 0
+            for server in servers:
+                price += server.server.price_daily if server.payment_duration == 'daily' else server.server.price_monthly
+            
+            if price > amount:
+                print(user.phone, amount, price)
     sleep(300)
