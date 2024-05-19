a5a743587c32594e657ef599851d34d6ee5ffbb7
Your Name <you@example.com>
2024-05-19 18:21:51 +0330

    fixed bugs

--- cloud/complete_server.py
+++ cloud/complete_server.py
@@ -24,7 +24,7 @@ async def get_data_server(server: ServerRent):
     if 'Windows' in server.os.name:
         await asyncio.sleep(700)
     elif server.location.city == 'Dubai':
-        await asyncio.sleep(200)
+        await asyncio.sleep(150)
     else:
         await asyncio.sleep(50)
     response = requests.get(
